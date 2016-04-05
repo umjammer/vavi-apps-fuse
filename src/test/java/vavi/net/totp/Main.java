da1d45c47e296fe0b59d4109cf1d00af5e529218
Naohide Sano <nsano@nsanomac3.local>
2016-04-05 11:45:35 +0800

    update totp

--- src/test/java/vavi/net/totp/Main.java
+++ src/test/java/vavi/net/totp/Main.java
@@ -7,32 +7,41 @@ package vavi.net.totp;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import vavi.net.totp.PinGenerator;
+import vavi.util.properties.annotation.Property;
+import vavi.util.properties.annotation.PropsEntity;
 
 
 /**
  * display counter with dots (30 second refresh)
  * 
- * $ java -classpath ./ vavi.net.totp.Main base32_encoded_secret
+ * $ java -cp ... vavi.net.totp.Main domain email
  * 
  * @author jcuff@srv
  */
+@PropsEntity(url = "file://${user.home}/.vavifuse/totp.properties")
 public class Main {
+    
+    @Property(name = "{0}.{1}")
+    transient String secret;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("\nAuthenticator Started!");
         System.out.println(":----------------------------:--------:");
         System.out.println(":       Code Wait Time       :  Code  :");
         System.out.println(":----------------------------:--------:");
 
-        final String secret = args[0];
+        final String domain = args[0];
+        final String email = args[1];
+        
+        Main app = new Main();
+        PropsEntity.Util.bind(app, domain, email);
 
         new Timer().scheduleAtFixedRate(new TimerTask() {
             int count = 1;
             String previouscode = "";
             /* */
             public void run() {
-                String newout = PinGenerator.computePin(secret, null);
+                String newout = PinGenerator.computePin(app.secret, null);
                 if (previouscode.equals(newout)) {
                     System.out.print(".");
                 } else {
