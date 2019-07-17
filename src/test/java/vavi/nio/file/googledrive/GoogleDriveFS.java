bd241067e859a5dcc5dea2f6eb7e433de6d7e272
umjammer <umjammer@gmail.com>
2019-07-18 06:56:47 +0900

    generalize fuse-jna to java nio file

---
+++ src/test/java/vavi/nio/file/googledrive/GoogleDriveFS.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package vavi.nio.file.googledrive;
+
+import java.net.URI;
+import java.nio.file.FileSystem;
+import java.util.HashMap;
+import java.util.Map;
+
+import vavi.net.fuse.JavaFsFS;
+
+
+/**
+ * GoogleDriveFS.
+ *
+ * @depends "file://${HOME}.vavifuse/googledrive/?"
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2016/02/29 umjammer initial version <br>
+ */
+public class GoogleDriveFS {
+    /**
+     * @param args 0: mount point, 1: email
+     */
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: GoogleDriveFS <mountpoint> <email>");
+            System.exit(1);
+        }
+
+        String email = args[1];
+
+        URI uri = URI.create("googledrive:///?id=" + email);
+
+        Map<String, Object> env = new HashMap<>();
+        env.put("ignoreAppleDouble", true);
+
+        FileSystem fs = new GoogleDriveFileSystemProvider().newFileSystem(uri, env);
+
+        new JavaFsFS(fs).mount(args[0]);
+    }
+}
