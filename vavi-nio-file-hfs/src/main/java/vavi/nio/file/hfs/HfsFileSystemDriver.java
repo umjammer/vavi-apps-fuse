65bb2a1755bfbfc286331d36b64f9194a45e31d1
umjammer <umjammer@gmail.com>
2020-06-04 11:27:01 +0900

    follow vavi-nio-file updating

--- vavi-nio-file-hfs/src/main/java/vavi/nio/file/hfs/HfsFileSystemDriver.java
+++ vavi-nio-file-hfs/src/main/java/vavi/nio/file/hfs/HfsFileSystemDriver.java
@@ -130,7 +130,7 @@ public final class HfsFileSystemDriver extends UnixLikeFileSystemDriverBase {
     public SeekableByteChannel newByteChannel(Path path,
                                               Set<? extends OpenOption> options,
                                               FileAttribute<?>... attrs) throws IOException {
-        if (options != null && (options.contains(StandardOpenOption.WRITE) || options.contains(StandardOpenOption.APPEND))) {
+        if (options != null && Util.isWriting(options)) {
             return new Util.SeekableByteChannelForWriting(newOutputStream(path, options)) {
                 @Override
                 protected long getLeftOver() throws IOException {
