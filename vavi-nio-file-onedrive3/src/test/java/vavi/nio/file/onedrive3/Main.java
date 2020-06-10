c90b8f7a72a4fb25887dfcbe52564c4cbd899f79
umjammer <umjammer@gmail.com>
2020-06-10 17:56:37 +0900

    update about onedrive3 test

--- vavi-nio-file-onedrive3/src/test/java/vavi/nio/file/onedrive3/Main.java
+++ vavi-nio-file-onedrive3/src/test/java/vavi/nio/file/onedrive3/Main.java
@@ -30,31 +30,6 @@ import static vavi.nio.file.Base.testAll;
  */
 public class Main {
 
-    public static void main(final String... args) throws IOException {
-        String email = args[1];
-
-        // Create the necessary elements to create a filesystem.
-        // Note: the URI _must_ have a scheme of "onedrive", and
-        // _must_ be hierarchical.
-        URI uri = URI.create("onedrive:///?id=" + email);
-
-        OAuth2AppCredential appCredential = new MicrosoftGraphLocalAppCredential();
-
-        Map<String, Object> env = new HashMap<>();
-        env.put(OneDriveFileSystemProvider.ENV_APP_CREDENTIAL, appCredential);
-        env.put("ignoreAppleDouble", true);
-
-        FileSystem fs = new OneDriveFileSystemProvider().newFileSystem(uri, env);
-
-        Map<String, Object> options = new HashMap<>();
-        options.put("fsname", "onedrive_fs" + "@" + System.currentTimeMillis());
-        options.put("noappledouble", null);
-        options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_DEBUG, true);
-        options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_READ_ONLY, false);
-
-        Fuse.getFuse().mount(fs, args[0], options);
-    }
-
     @Test
     void test01() throws Exception {
         String email = System.getenv("MICROSOFT3_TEST_ACCOUNT");
--- vavi-nio-file-onedrive3/src/test/java/vavi/nio/file/onedrive3/Main4.java
+++ vavi-nio-file-onedrive3/src/test/java/vavi/nio/file/onedrive3/Main4.java
@@ -6,6 +6,7 @@
 
 package vavi.nio.file.onedrive3;
 
+import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.util.HashMap;
@@ -16,7 +17,10 @@ import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import vavi.net.auth.oauth2.OAuth2AppCredential;
+import vavi.net.auth.oauth2.microsoft.MicrosoftGraphLocalAppCredential;
 import vavi.net.fuse.Base;
+import vavi.net.fuse.Fuse;
 
 
 /**
@@ -66,6 +70,33 @@ public class Main4 {
         Base.testFuse(fs, mountPoint, options);
 
         fs.close();
+    }
+
+    //
+
+    public static void main(final String... args) throws IOException {
+        String email = args[1];
+
+        // Create the necessary elements to create a filesystem.
+        // Note: the URI _must_ have a scheme of "onedrive", and
+        // _must_ be hierarchical.
+        URI uri = URI.create("onedrive:///?id=" + email);
+
+        OAuth2AppCredential appCredential = new MicrosoftGraphLocalAppCredential();
+
+        Map<String, Object> env = new HashMap<>();
+        env.put(OneDriveFileSystemProvider.ENV_APP_CREDENTIAL, appCredential);
+        env.put("ignoreAppleDouble", true);
+
+        FileSystem fs = new OneDriveFileSystemProvider().newFileSystem(uri, env);
+
+        Map<String, Object> options = new HashMap<>();
+        options.put("fsname", "onedrive_fs" + "@" + System.currentTimeMillis());
+        options.put("noappledouble", null);
+        options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_DEBUG, true);
+        options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_READ_ONLY, false);
+
+        Fuse.getFuse().mount(fs, args[0], options);
     }
 }
 
