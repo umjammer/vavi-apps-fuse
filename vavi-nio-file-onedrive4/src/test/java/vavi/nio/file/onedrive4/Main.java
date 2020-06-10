e3fcf0f55da63ebc55e6dc200c777243868ce5e3
umjammer <umjammer@gmail.com>
2020-06-10 17:57:03 +0900

    update about onedrive4 test

--- vavi-nio-file-onedrive4/src/test/java/vavi/nio/file/onedrive4/Main.java
+++ vavi-nio-file-onedrive4/src/test/java/vavi/nio/file/onedrive4/Main.java
@@ -6,20 +6,15 @@
 
 package vavi.nio.file.onedrive4;
 
-import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import vavi.net.fuse.Fuse;
 
 import static vavi.nio.file.Base.testAll;
 
@@ -31,25 +26,6 @@ import static vavi.nio.file.Base.testAll;
  * @version 0.00 2016/03/xx umjammer initial version <br>
  */
 public class Main {
-
-    public static void main(final String... args) throws IOException {
-        String email = args[1];
-
-        Map<String, Object> env = new HashMap<>();
-        env.put("ignoreAppleDouble", true);
-
-        URI uri = URI.create("onedrive:///?id=" + email);
-
-        FileSystem fs = new OneDriveFileSystemProvider().newFileSystem(uri, env);
-
-        Map<String, Object> options = new HashMap<>();
-        options.put("fsname", "onedrive_fs" + "@" + System.currentTimeMillis());
-        options.put("noappledouble", null);
-        options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_DEBUG, true);
-        options.put(vavi.net.fuse.javafs.JavaFSFuse.ENV_READ_ONLY, false);
-
-        Fuse.getFuse().mount(fs, args[0], options);
-    }
 
     @Test
     void test01() throws Exception {
